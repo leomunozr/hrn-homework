85133fe6797070cbceb41796b0bccbbe59ce66bf
leomunozr <leonardomr1@gmail.com>
2017-08-04 15:49:50 -0500

    reload gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,23 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('buildStyles', function() {
   return gulp.src('./styles.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
   gulp.watch('./styles.scss', ['buildStyles'])
 });
 
-gulp.task('default', ['buildStyles', 'watch']);
+gulp.task('reload', function() {
+  browserSync.init({
+    server: './'
+  });
+})
+
+gulp.task('default', ['reload','buildStyles', 'watch']);
