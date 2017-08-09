33d270c05db1486d3d2d67d097bd17b505a68aec
Leonardo Munoz <leonardomr1@gmail.com>
2017-08-08 23:24:16 -0500

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var cleanCss = require('gulp-clean-css');
+
+var cleanCssOptions = {
+  level: 2
+}
 
 gulp.task('buildStyles', function() {
-  return gulp.src('./styles.scss')
+  return gulp.src('*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCss({
+
+
+    }))
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
 });
